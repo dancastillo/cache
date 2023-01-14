2d3e6ac96ea9d100b4fc5256709678ee87fad7b8
dan.castillo <dancastillo99@gmail.com>
2023-01-13 19:33:08 -0600

    feat: added cache logic
    Signed-off-by: dan.castillo <dancastillo99@gmail.com>

--- imcache.d.ts
+++ imcache.d.ts
@@ -1,20 +1,20 @@
 export type DurationOptions = {
-  days?: number;
-  hours?: number;
-  minutes?: number;
-  seconds?: number;
+  days?: number
+  hours?: number
+  minutes?: number
+  seconds?: number
 }
 
 /**
  * @return [number] the current time in milliseconds
  */
-export type GetTimeFn = () => number;
+export type GetTimeFn = () => number
 
 /**
  * @param [string] key to get value from
  * @return [any]
  */
-export type GetFn = (key: string) => any;
+export type GetFn = (key: string) => any
 
 /**
  * @param [string] key to store value under
@@ -22,43 +22,43 @@ export type GetFn = (key: string) => any;
  * @param [DurationOptions] time duration to store value under key
  * @returns [void]
  */
-export type PutFn = (key: string, value: any, time?: DurationOptions) => void;
+export type PutFn = (key: string, value: any, time?: DurationOptions) => void
 
 /**
  * @param [string] key to delete value from
  * @returns [void]
  */
-export type DelFn = (key: string) => void;
+export type DelFn = (key: string) => void
 
 /**
  * @return [void] clear all keys from cache
  */
-export type ClearFn = () => void;
+export type ClearFn = () => void
 
 /**
  * @return [string[]] all keys in cache
  */
-export type KeysFn = () => string[];
+export type KeysFn = () => string[]
 
 export type imcacheInstance = {
-  ttl: number | undefined;
-  getTime: GetTimeFn;
-  get: GetFn;
-  put: PutFn;
-  del: DelFn;
-  clear: ClearFn;
-  keys: KeysFn;
+  ttl: number | undefined
+  getTime: GetTimeFn
+  get: GetFn
+  put: PutFn
+  del: DelFn
+  clear: ClearFn
+  keys: KeysFn
 }
 
 export type Options = {
-  duration?: DurationOptions;
+  duration?: DurationOptions
 }
 
 declare namespace imcache {
 
 
   export interface Options {
-    duration?: DurationOptions;
+    duration?: DurationOptions
   }
 
   export const Options: Options
---
+++ imcache.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const { duration } = require('./lib/duration')
+const { validate } = require('./lib/validate')
+
+const ARGS_SCHEMA = {
+  type: 'object',
+  properties: {
+    duration: { type: 'object' }
+  }
+}
+
+function imcache (opts) {
+  opts = opts || {}
+
+  validate(opts, ARGS_SCHEMA)
+
+  const ttl = duration(opts.duration)
+
+  const _cache = {}
+
+  const getTime = function _getTime () {
+    return new Date().getTime()
+  }
+
+  const get = function _get (key) {
+    const data = _cache[key]
+    if (!data || !data.value || data.expires < this.getTime()) {
+      return
+    }
+
+    return data.value
+  }
+  const put = function (key, value, time) {
+    let expires = this.getTime() + ttl
+    if (time) {
+      // let user define ttl duration for custom keys
+      expires = this.getTime() + duration(time)
+    }
+
+    _cache[key] = {
+      value,
+      expires
+    }
+  }
+
+  const del = function (key) {
+    delete _cache[key]
+  }
+
+  const clear = function () {
+    for (const key in _cache) {
+      delete _cache[key]
+    }
+  }
+
+  const keys = function () {
+    return Object.keys(_cache)
+  }
+  const imcache = {
+    ttl,
+    getTime,
+    get,
+    put,
+    del,
+    clear,
+    keys
+  }
+
+  return imcache
+}
+
+imcache.default = imcache
+imcache.imcache = imcache
+module.exports = imcache
+// module.exports.default = imcache
+// module.exports.imcache = imcache
