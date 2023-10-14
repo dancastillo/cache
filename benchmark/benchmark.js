a698015b6527f58ef041c3c86dcf824f49015e3c
dan.castillo <dan.castillo@jasper.ai>
2023-10-14 13:10:06 -0400

    add more benchmarks

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,38 +1,57 @@
 'use strict'
 
+const { randomUUID } = require('node:crypto')
+const { hrtime } = require('node:process')
 const c = require('../cache')
 
-const runBenchmark = (cache, iterations) => {
-  const start = Date.now()
+const getSetValue = (type) => {
+  if (type === 'string') {
+    return randomUUID()
+  }
+
+  if (type === 'object') {
+    return { value: randomUUID(), test: true, benchmark: true }
+  }
+
+  if (type === 'object-stringify') {
+    return JSON.stringify({ value: randomUUID(), test: true, benchmark: true })
+  }
+}
+
+const runBenchmarkString = (cache, iterations, type) => {
+  const start = hrtime.bigint()
 
   // Benchmark set operation
   for (let i = 0; i < iterations; i++) {
-    cache.set(`key_${i}`, i)
+    const value = getSetValue(type)
+    cache.set(`key_${i}`, value)
   }
 
-  const setDuration = Date.now() - start
+  const setDuration = hrtime.bigint() - start
 
   // Benchmark get operation
-  const getStart = Date.now()
+  const getStart = hrtime.bigint()
   for (let i = 0; i < iterations; i++) {
     cache.get(`key_${i}`)
   }
 
-  const getDuration = Date.now() - getStart
+  const getDuration = hrtime.bigint() - getStart
 
   // Benchmark delete operation
-  const deleteStart = Date.now()
+  const deleteStart = hrtime.bigint()
   for (let i = 0; i < iterations; i++) {
     cache.del(`key_${i}`)
   }
 
-  const deleteDuration = Date.now() - deleteStart
+  const deleteDuration = hrtime.bigint() - deleteStart
 
-  console.log(`Benchmarked ${iterations} iterations}`)
-  console.log(`Set operation took ${setDuration}ms`)
-  console.log(`Get operation took ${getDuration}ms`)
-  console.log(`Delete operation took ${deleteDuration}ms`)
+  console.log(`Benchmarked ${iterations} iterations of ${type} values`)
+  console.log(`Set operation took ${setDuration} nanoseconds`)
+  console.log(`Get operation took ${getDuration} nanoseconds`)
+  console.log(`Delete operation took ${deleteDuration} nanoseconds\n`)
 }
 
 // Run the benchmark with 1 million iterations
-runBenchmark(c({ max: 1000000 }), 1000000)
+runBenchmarkString(c({ max: 1000000 }), 1000000, 'string')
+runBenchmarkString(c({ max: 1000000 }), 1000000, 'object')
+runBenchmarkString(c({ max: 1000000 }), 1000000, 'object-stringify')
